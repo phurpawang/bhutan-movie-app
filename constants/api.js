34b096cd9bb2dc473032ab1dca82fff6888773af
phurpa <12002001841@rim.edu.bt>
2025-12-10 12:08:54 +0600

    Update API to use Render cloud backend

--- constants/api.js
+++ constants/api.js
@@ -2,4 +2,4 @@
 export const API_KEY = "e1559ac10c39e3c2df64b77c4f82fc2a"; // Replace with your TMDB API Key
 export const BASE_URL = "https://api.themoviedb.org/3";
 export const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
-export const BACKEND_API_URL = "http://192.168.2.219:5001/api"; // Update with your local IP if testing on device
+export const BACKEND_API_URL = "https://bhutan-movie-app.onrender.com/api"; // Cloud backend - works 24/7!
